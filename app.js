32f32b5c084eecff9a54ea5dd92c1bdbe8ab39ac
tkuchar <tkuchar84@gmail.com>
2019-02-20 07:47:07 -0800

    event delegation fix

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const open_btn = document.getElementById("open");
 const modal = document.getElementById("modal");
 const close = document.getElementById("close")
 const grid = document.querySelector(".gallery");
-const imgCollect = document.getElementsByClassName("gallery__item");
-const imgArr = Array.from(imgCollect);
+// const imgCollect = document.getElementsByClassName("gallery__item");
+// const imgArr = Array.from(imgCollect);
 
 // https://github.com/aholachek/animate-css-grid
 if(error === false) {
   animateCSSGrid.wrapGrid(grid, {duration : 300});
 }
 
-imgArr.forEach(function(img) {
+/*imgArr.forEach(function(img) {
   img.addEventListener('click', function() {
     img.classList.toggle("view-center");
   })
-});
+});*/
+
+// Event delegation for gallery
+
+const gridAnimate = (e) => {
+  if (e.target && e.target.nodeName === 'IMG') {
+    e.target.parentNode.classList.toggle("view-center");
+  }
+}
+
+document.querySelector('.gallery').addEventListener('click', gridAnimate);
 
 open_btn.addEventListener("click", function() {
   modal.style.transition = "all .5s ease-in";
